d3787fd280c242b62282e457ef1c4bb0eca15700
devstarks <devstarks@gmail.com>
2016-10-04 10:31:03 -0400

    finishes gemfile/webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/slakk.jsx",
+  output: {
+    path: "./app/assets/javascripts",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
