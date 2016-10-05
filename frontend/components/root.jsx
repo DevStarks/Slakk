1c00cac8555b62079a6d4b0523f2bdf1872056b2
devstarks <devstarks@gmail.com>
2016-10-04 23:04:50 -0400

    pre merge commit

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './app';
-import LoginPage from './login/login_page'
-import SignupPage from './signup/signup_page'
+import LoginPage from './login/login_page';
+import SignupPage from './signup/signup_page';
 
 const Root = ({ store }) => {
   return (
