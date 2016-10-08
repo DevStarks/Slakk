d7345f83247202cce6097ca8f5eb0c9f2ae52021
App Academy Student <devstarks@gmail.com>
2016-10-07 22:35:12 -0400

    finishes css for messages

--- frontend/components/messages/message.jsx
+++ frontend/components/messages/message.jsx
@@ -13,10 +13,16 @@ class Message extends React.Component {
     this.handleEditClick = this.handleEditClick.bind(this);
     this.handleDeleteClick = this.handleDeleteClick.bind(this);
     this.unmountEdit = this.unmountEdit.bind(this);
+    this.menuToggle = this.menuToggle.bind(this);
 
     this.state = {
       type: "show"
     };
+  }
+
+  componentDidMount() {
+
+    this.refs.messageActions.style.display = "none";
   }
 
   handleEditClick() {
@@ -31,17 +37,24 @@ class Message extends React.Component {
     this.setState({ type: "show" });
   }
 
+  menuToggle() {
+
+    this.refs.messageActions.style.display = "block";
+  }
+
   render() {
     const { info: { body, created_at, author } } = this.props;
 
     if (this.state.type === "show") {
       return (
-        <article className="message-content">
+        <article className="message-content group">
           <img className="author-thumbnail" src={author.image_url}></img>
-          <h3>{author.username} {timeFromDateObj(created_at)}</h3>
+          <h3>{author.username} </h3>
+          <span>{timeFromDateObj(created_at)}</span>
+          <br/>
           <p>{body}</p>
-
-          <ul className="message-actions">
+          <button onClick={this.menuToggle}>...</button>
+          <ul ref="messageActions" className="message-actions">
             <li onClick={this.handleEditClick}>Edit message</li>
             <li onClick={this.handleDeleteClick}>Delete message</li>
           </ul>
